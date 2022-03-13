3ede7e158f2252b30630c76a0d74c0bd39e3d816
jian-ge-tai-da <1519162203@qq.com>
2022-03-13 19:30:31 +0800

    create linkedList

--- Chapter2/LinkedList.h
+++ Chapter2/LinkedList.h
@@ -4,15 +4,15 @@
 
 #ifndef DATASTRUCTUREIMPLEMENTINGC_LINKEDLIST_H
 #define DATASTRUCTUREIMPLEMENTINGC_LINKEDLIST_H
-#include <stdlib.h>
+
 #include <errno.h>
-typedef int elementType;
+typedef int ELEMENT_TYPE;
 typedef struct lNode{
-    elementType data;
-    struct elementType* next;
+    ELEMENT_TYPE data;
+    struct lNode* next;
 } lNode, *linkedList;
 
-__attribute__((unused)) linkedList lListInit(){
+linkedList lListInit(){
     lNode *p = (lNode*)malloc(sizeof(lNode));
     if(!p)abort(1);
     p->next = NULL;
@@ -20,12 +20,60 @@ __attribute__((unused)) linkedList lListInit(){
     return p;
 }
 
-__attribute__((unused)) void lListDestroyRegress(linkedList l) {
+linkedList lListInitByLen(int len){
+    lNode *p = (lNode*)malloc(sizeof(lNode));
+    if(!p)abort(1);
+    p->data = len;
+    lNode *r = p;
+    int i = 1;
+    while(i<=len){
+        r->next = (lNode*)malloc(sizeof(lNode));
+        r = r->next;
+    }
+    r->next = NULL;
+    return p;
+}
+
+void lListDestroyRegress(linkedList l) {
     if(l == NULL){
         return;
     }else{
-
+        lListDestroyRegress(l->next);
+        free(l);
     }
+}
+
+void lListInsertRear(lNode *p, ELEMENT_TYPE item){
+    lNode *r = (lNode*)malloc(sizeof(lNode));
+    r->next = p->next;
+    r->data = item;
+    p->next = r;
+}
+
+void lListInsertPrior(lNode *p, ELEMENT_TYPE item){
+    lNode *r = (lNode*)malloc(sizeof(lNode));
+    r->next = p->next;
+    p->next = r;
+    r->data = p->data;
+    p->data = item;
+}
+
+void lListInsertById(linkedList p, int pos,ELEMENT_TYPE item){
+    if(pos>p->data){
+        perror("exceed boundary");
+
+        abort(1);
+    }
+    for(int i=1;i<=pos;i++){
+        p = p->next;
+    }
+    lListInsertRear(p, item);
+}
+
+void deleteRearByNode(lNode *p){
+    if(!p->next)return;
+    lNode *r = (p->next)->next;
+    free(p->next);
 }
 
 
