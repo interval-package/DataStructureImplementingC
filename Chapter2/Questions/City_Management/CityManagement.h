5358edbca9d06acfe8e2b07d7cf4bdef8e8da83f
Zza <1519162203@qq.com>
2022-03-20 21:35:13 +0800

    city management end

--- Chapter2/Questions/City_Management/CityManagement.h
+++ Chapter2/Questions/City_Management/CityManagement.h
@@ -7,6 +7,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define NAME_LEN 20
+#define INFO_LEN 30
 
 // 使用双向链表
 typedef struct city{
@@ -17,8 +21,8 @@ typedef struct city{
     };
 
     struct city *next, *before;
-    char *info;
-    char *name;
+    char info[INFO_LEN];
+    char name[NAME_LEN];
 }city, *cityList;
 
 cityList InitCityList(){
@@ -26,12 +30,25 @@ cityList InitCityList(){
     res->before = NULL;
     res->next = NULL;
     res->len = 0;
+    strcpy(res->info,"no description");
+    strcpy(res->name,"start of list");
     return res;
 }
 
-void AddCity(cityList cl){
+void DestructCity(cityList cl){
+    cityList temp;
+    while(cl){
+        temp = cl->next;
+        free(cl);
+        cl = temp;
+    }
+}
+
+void AddCity(cityList cl, const char* name, const char* info){
     cityList p = (cityList)malloc(sizeof(city));
 //    city id 由我们分配,对于表为空,那么id设置为1
+    strcpy(p->name,name);
+    strcpy(p->info,info);
     p->cityId = cl->next?cl->next->cityId+1:1;
 //    插入
     p->before = cl;
@@ -40,7 +57,7 @@ void AddCity(cityList cl){
     cl->next = p;
 }
 
-void DeleteCity(cityList cl, int id){
+void DeleteCity_Id(cityList cl, int id){
     do{
         cl = cl->next;
     }
@@ -54,13 +71,47 @@ void DeleteCity(cityList cl, int id){
 }
 
 void DispCity(cityList c){
+    printf("city id\t: %d\n"
+           "city name\t: %s\n"
+           "city info\t: %s\n",
+           c->cityId,c->name,c->info
+    );
+}
+
+void DispCity_All(cityList c){
     while (c->next){
         c = c->next;
-        printf("city id\t: %d\n"
-               "city name\t: %s\n"
-               "city info\t: %s\n",
-               c->cityId,c->name,c->info
-               );
+        DispCity(c);
+    }
+}
+
+int Comp_by_name(city* cl,city* tar){
+    return strcmp(cl->name,tar->name);
+}
+
+int Comp_by_id(city* cl,city* tar){
+    return cl->cityId == tar->cityId;
+}
+
+cityList FindCity(cityList cl, city *tar,int(*comp)(city*,city*)){
+//    comp的结果是真为非0，假为0
+//    返回的是结果的指针
+    do{
+        cl = cl->next;
+    }while(cl && !comp(cl,tar));
+//    没有找到就自然为NULL
+    return cl;
+}
+
+void UpdateCity(cityList cl, int id, const char* name, const char* info){
+    do{
+        cl = cl->next;
+    }
+    while (cl && cl->cityId != id);
+//    对于双链表的删除和插入
+    if(cl){
+        if(name)strcpy(cl->name,name);
+        if(info)strcpy(cl->info,info);
     }
 }
 
--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 #Data structure class Note
-##introduction
-It's the note I took at my class. 
-##tutorial
+## (1)introduction
+#### 1.It's the note I took at my class. 
+It basically contains all the points of our course and homeworks.
+#### 2.Algorithm
+It may have some algorithm problems, and I'll share my solution of them.
+## (2)tutorial
+#### 1.Chapters
+It actually totally same as the book.
+#### 2.Detail info
+For each Chapter, I'd write a .md file to illustrate the questions of this chapter.
 
-##acknowledgement
+And in come chapter may have my class note.
+## (3)acknowledgement
+Thanks myself, for getting increment every day.
