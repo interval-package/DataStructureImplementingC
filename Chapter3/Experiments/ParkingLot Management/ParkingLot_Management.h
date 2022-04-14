c5264c65e76845f4c99263f1452caa5fd5c97599
Zza <1519162203@qq.com>
2022-04-14 13:36:59 +0800

    ParkingLot_Management.h

--- Chapter3/Experiments/ParkingLot Management/ParkingLot_Management.h
+++ Chapter3/Experiments/ParkingLot Management/ParkingLot_Management.h
@@ -5,4 +5,60 @@
 #ifndef DATASTRUCTUREIMPLEMENTINGC_PARKINGLOT_MANAGEMENT_H
 #define DATASTRUCTUREIMPLEMENTINGC_PARKINGLOT_MANAGEMENT_H
 
+typedef struct Car{
+    int id;
+    int time;
+} Car, *pCar;
+
+typedef Car ELEMENT_TYPE;
+#ifndef ELEM
+#define ELEM
+#endif
+
+typedef struct ParkingLot{
+
+} ParkingLot, *pPark;
+
+typedef struct Dispatch{
+    char act;
+    int id;
+    int time;
+} Dispatch;
+
+#include "../../Stack.h"
+
+int Parking_Dispatch(pPark, Dispatch *);
+
+void Display_Cur_Status(pPark);
+
+pPark InitPark();
+
+void test_main(){
+//    初始化数据
+    int n = 2;
+    Dispatch actions[] = {
+            {'A',1,5},
+            {'A',2,10},
+            {'D',1,15},
+            {'A',3,20},
+            {'A',4,25},
+            {'A',5,30},
+            {'D',2,35},
+            {'D',4,40},
+            {'E',0,0}
+    };
+    Dispatch *CurAct = actions;
+    pPark park = InitPark();
+
+    while(CurAct->act != 'E'){
+        Parking_Dispatch(park, CurAct);
+        Display_Cur_Status(park);
+        CurAct++;
+    }
+}
+
+int Parking_Dispatch(pPark tar, Dispatch *actions){
+
+}
+
 #endif //DATASTRUCTUREIMPLEMENTINGC_PARKINGLOT_MANAGEMENT_H
--- main.c
+++ main.c
@@ -4,23 +4,14 @@
 //#include "Chapter2/Questions/SortByAccessFrequency/SortByAccessFrequency.h"
 //#include "Chapter2/Questions/City_Management/CityManagement.h"
 //#include "Chapter2/LinkedList.h"
-
 //#include "Chapter3/Questions/RPN/RPN.h"
-#include "Chapter3/Questions/TrainTrans/TrainTrans.h"
-
-#include "Chapter4/strStr/strStr.h"
-
-#include "Chapter6/Binary Tree/Rebuild_BinTree/PreOrderTransverse.h"
-
+//#include "Chapter3/Questions/TrainTrans/TrainTrans.h"
+//#include "Chapter4/strStr/strStr.h"
+//#include "Chapter6/Binary Tree/Rebuild_BinTree/PreOrderTransverse.h"
 //#include "External Problems/SelfDivide/SelfDivideNumbers.h"
 //#include "External Problems/SleepingBarber/Sleeping_Barber.h"
 
 
 int main(){
-//    JosephusProblem_Main();
-//    printf("%d",isSelfDivide(21));
-//    main_barber_problem(0,NULL);
-//    PreOrder_Main();
-    printf("%d", strStr("wqerewetr","er"));
     return 0;
 }
