a412d3a4091099e457c0d477974ba004dc7a1181
Zza <1519162203@qq.com>
2022-05-17 22:01:14 +0800

    traverse

--- Chapter7/Experiment/Traveling Salesman Problem/TSP.h
+++ Chapter7/Experiment/Traveling Salesman Problem/TSP.h
@@ -12,29 +12,38 @@
 
 //核心函数，求出动态规划dp数组
 void TSP(int** dp, int** g){
-    //初始化dp[i][0]
+// dp为返回参数
+// g 为我们已经有的路径数组
+
+    // 初始化dp[i][0]
     for(int i = 0 ; i < N ;i++){
         dp[i][0] = g[i][0];
     }
-    //求解dp[i][j],先跟新列在更新行
+    // 求解dp[i][j],先更新列再更新行
     for(int j = 1 ; j < M ;j++){
         for(int i = 0 ; i < N ;i++ ){
+//            先假定自己到达这个集为不可能
             dp[i][j] = INF;
-            //如果集和j(或状态j)中包含结点i,则不符合条件退出
+            // 如果集和j(或状态j)中包含结点i,则不符合条件退出
             if( ((j >> (i-1)) & 1) == 1){
                 continue;
             }
+//            取最小的子
             for(int k = 1 ; k < N ; k++){
                 if( ((j >> (k-1)) & 1) == 0){
+//                    如果j中无k
                     continue;
                 }
-                if( dp[i][j] > g[i][k] + dp[k][j^(1<<(k-1))]){
+                if( dp[i][j] > g[i][k] + dp[k][j^(1<<(k-1))] ){
+//                    j^(1<<(k-1))表示将1移到第k位，也就是指向只有k元素的这个集，然后异或，表示剔除这个k
+//                    也就是构造 cij = cik + dp{j - k}
                     dp[i][j] = g[i][k] + dp[k][j^(1<<(k-1))];
                 }
             }
         }
     }
-
 }
+
+
 
 #endif //DATASTRUCTUREIMPLEMENTINGC_TSP_H
